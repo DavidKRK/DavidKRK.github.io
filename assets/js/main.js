297912a78039c4dc53b0becccddd28ec4e9f790c
DavidKRK <136035295+DavidKRK@users.noreply.github.com>
2025-08-10 23:11:18 +0200

    Update main.js (#18)

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,24 @@
-const img = document.getElementById('hero-img');
-const hero = document.getElementById('hero');
+var sound = new Howl({
+  src: ['assets/music/New Order Blue Monday.ogg', 'assets/music/New Order Blue Monday.mp3'], // Assurez-vous d'inclure différents formats pour la compatibilité
+  preload: true,
+  volume: 0.5, // Ajustez le volume ici (0.0 à 1.0)
+  onload: function() {
+    console.log('Son chargé avec succès !');
+  },
+  onloaderror: function() {
+    console.log('Erreur lors du chargement du son.');
+  }
+});
+
+// Pour démarrer le son
+function playSound() {
+  sound.play();
+}
+
+//Pour mettre en pause le son
+function pauseSound() {
+  sound.pause();
+}
 
 window.addEventListener('scroll', () => {
   if (window.innerWidth < 768) {
