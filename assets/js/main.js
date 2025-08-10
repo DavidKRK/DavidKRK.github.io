3fc9c086828790f29327e959412e21fadbf1fcbe
DavidKRK <136035295+DavidKRK@users.noreply.github.com>
2025-08-11 01:35:15 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,24 +1,52 @@
-var sound = new Howl({
-  src: ['assets/music/New Order Blue Monday.ogg']
+var patriceSound = new Howl({
+  src: ['/workspaces/DavidKRK.github.io/assets/music/Patrice Bäumel–Glutes (Original Mix).mp3']
 });
 
-document.getElementById('play-btn-blue-monday').addEventListener('click', () => sound.play());
-document.getElementById('pause-btn-blue-monday').addEventListener('click', () => sound.pause());
+var newOrderSound = new Howl({
+  src: ['/workspaces/DavidKRK.github.io/assets/music/New Order Blue Monday.ogg']
+});
 
-// Pour démarrer le son
-function playSound() {
-  sound.play();
+var winxSound = new Howl({
+  src: ['/workspaces/DavidKRK.github.io/assets/music/Winx - Don\'t Laugh - Original Live Raw Mix.ogg']
+});
+
+document.getElementById('play-btn-patrice').addEventListener('click', () => patriceSound.play());
+document.getElementById('pause-btn-patrice').addEventListener('click', () => patriceSound.pause());
+
+document.getElementById('play-btn-blue-monday').addEventListener('click', () => newOrderSound.play());
+document.getElementById('pause-btn-blue-monday').addEventListener('click', () => newOrderSound.pause());
+
+document.getElementById('play-btn-winx').addEventListener('click', () => winxSound.play());
+document.getElementById('pause-btn-winx').addEventListener('click', () => winxSound.pause());
+
+// Pour démarrer les sons
+function playPatriceSound() {
+  patriceSound.play();
 }
 
-//Pour mettre en pause le son
-function pauseSound() {
-  sound.pause();
+function playNewOrderSound() {
+  newOrderSound.play();
+}
+
+function playWinxSound() {
+  winxSound.play();
+}
+
+// Pour mettre en pause les sons
+function pausePatriceSound() {
+  patriceSound.pause();
+}
+
+function pauseNewOrderSound() {
+  newOrderSound.pause();
+}
+
+function pauseWinxSound() {
+  winxSound.pause();
 }
 
 window.addEventListener('scroll', () => {
   if (window.innerWidth < 768) {
-@@ -17,4 +36,4 @@ window.addEventListener('scroll', () => {
-
-    img.style.transform = `translate(-50%, calc(-50% + ${translateY}px)) scale(${scale})`;
+    // ...
   }
 });
